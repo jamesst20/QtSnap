84e828cfb181953eb920a4403bcbf9b0ebb005b6
James St-Pierre <Jamesst20@gmail.com>
2014-12-20 14:17:46 -0500

    New small demo (Support Logging in)

--- QtSnap-DemoApp/main.cpp
+++ QtSnap-DemoApp/main.cpp
@@ -1,8 +1,33 @@
 #include <QCoreApplication>
 
+#include "Snapchat.h"
+
+void onLoginCompletted(bool success, QString reason){
+    qDebug() << "Success : " << success << " Reason : " << reason;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QTextStream in(stdin);
+    QTextStream out(stdout);
+
+    out << "Snapchat username : ";
+    out.flush();
+    QString username = in.readLine();
+
+    out << "Snapchat password : ";
+    out.flush();
+    QString password = in.readLine();
+
+    Snapchat snapchat;
+    QObject::connect(&snapchat, &Snapchat::loginCompleted, &onLoginCompletted);
+
+    out << "Logging in...\n";
+    out.flush();
+
+    snapchat.login(username, password);
 
     return a.exec();
 }
