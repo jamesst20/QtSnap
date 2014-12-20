beacfb8681cc28546843f39063e0f3a1ded1a16a
James St-Pierre <Jamesst20@gmail.com>
2014-12-20 14:20:29 -0500

    New Utils class. Added --> Logging to file

--- QtSnap-Library/Utils.cpp
+++ QtSnap-Library/Utils.cpp
@@ -1,12 +1,16 @@
 #include "Utils.h"
 
-Utils::Utils()
-{
+QFile *Utils::file_log = 0;
 
+void Utils::log(QByteArray bytes){
+    if (!file_log){
+        if (QFile::exists("./logs.txt")){
+            QFile::remove("./logs.txt");
+        }
+        file_log = new QFile("./logs.txt");
+        file_log->open(QIODevice::WriteOnly | QIODevice::Text);
+    }
+    QTextStream out(file_log);
+    out.setCodec("UTF-8");
+    out << bytes << "\n";
 }
-
-Utils::~Utils()
-{
-
-}
-
--- QtSnap-Library/Utils.h
+++ QtSnap-Library/Utils.h
@@ -1,12 +1,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <QFile>
+#include <QByteArray>
+#include <QTextStream>
 
 class Utils
 {
 public:
-    Utils();
-    ~Utils();
+    static void log(QByteArray bytes);
+
+private:
+    static QFile *file_log;
 };
 
 #endif // UTILS_H
