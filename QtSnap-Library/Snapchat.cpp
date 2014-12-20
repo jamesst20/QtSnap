143eb3e7a8f446722df3d89e0130fd8c4925f7c4
James St-Pierre <Jamesst20@gmail.com>
2014-12-20 14:19:58 -0500

    Snapchat login support

--- QtSnap-Library/Snapchat.cpp
+++ QtSnap-Library/Snapchat.cpp
@@ -1,6 +1,62 @@
 #include "Snapchat.h"
 
+const QString Snapchat::LOGIN_PATH = "loq/login";
+
+const QString Snapchat::UPDATES_RESPONSE_KEY = "updates_response";
+const QString Snapchat::FRIENDS_RESPONSE_KEY = "friends_response";
+const QString Snapchat::STORIES_RESPONSE_KEY = "stories_response";
+const QString Snapchat::CONVERSATIONS_REPONSE_KEY = "conversations_response";
+
+const QString Snapchat::USERNAME_KEY = "username";
+const QString Snapchat::PASSWORD_KEY = "password";
+const QString Snapchat::TIMESTAMP_KEY = "timestamp";
+const QString Snapchat::REQ_TOKEN_KEY = "req_token";
+const QString Snapchat::MESSAGE_KEY = "message";
+
 
 Snapchat::Snapchat()
 {
+
+}
+
+void Snapchat::login(QString username, QString password){
+    this->username = username;
+
+    QUrlQuery params;
+
+    long timestamp = QDateTime::currentMSecsSinceEpoch();
+    params.addQueryItem(USERNAME_KEY, username);
+    params.addQueryItem(PASSWORD_KEY, password);
+    params.addQueryItem(REQ_TOKEN_KEY, TokenLib::getStaticRequestToken(timestamp));
+    params.addQueryItem(TIMESTAMP_KEY, QString::number(timestamp));
+
+    NetworkRequestMaker *nrm = new NetworkRequestMaker(LOGIN_PATH, params);
+    connect(nrm, SIGNAL(onRequestDone(int,QByteArray)), this, SLOT(onLoginCompleted(int,QByteArray)));
+    nrm->executeRequest();
+
+}
+
+void Snapchat::onLoginCompleted(int httpCode, QByteArray data){
+    //Get the signal emitter (NetworkRequestMaker)
+    NetworkRequestMaker *nrm = qobject_cast<NetworkRequestMaker *>(sender());
+    //Delete it, free memory
+    nrm->deleteLater();
+
+    //Log data received
+    Utils::log(data);
+
+    QJsonParseError jsonError;
+    QJsonDocument fullSnapchatDoc = QJsonDocument::fromJson(data, &jsonError);
+
+    if(jsonError.error == QJsonParseError::NoError){
+        this->fullSnapchatObj = fullSnapchatDoc.object();
+
+        if(this->fullSnapchatObj.contains(UPDATES_RESPONSE_KEY)){
+            emit loginCompleted(true, "");
+        }else{
+            emit loginCompleted(false, fullSnapchatObj.contains(MESSAGE_KEY) ? fullSnapchatObj["message"].toString() : "Unknown error");
+        }
+    }else{
+        emit loginCompleted(false, "Json Parser error : " + jsonError.errorString());
+    }
 }
--- QtSnap-Library/Snapchat.h
+++ QtSnap-Library/Snapchat.h
@@ -1,12 +1,46 @@
 #ifndef SNAPCHAT_H
 #define SNAPCHAT_H
 
+#include <QString>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
 
-class Snapchat
+#include "TokenLib.h"
+#include "Utils.h"
+#include "NetworkRequestMaker.h"
+
+
+class Snapchat : public QObject
 {
-
+    Q_OBJECT
 public:
     Snapchat();
+    void login(QString username, QString password);
+
+private slots:
+    void onLoginCompleted(int httpCode, QByteArray data);
+
+signals:
+    void loginCompleted(bool success, QString reason);
+
+private:
+    QString username;
+
+    QJsonObject fullSnapchatObj;
+
+    static const QString LOGIN_PATH;
+
+    static const QString UPDATES_RESPONSE_KEY;
+    static const QString FRIENDS_RESPONSE_KEY;
+    static const QString STORIES_RESPONSE_KEY;
+    static const QString CONVERSATIONS_REPONSE_KEY;
+
+    static const QString USERNAME_KEY;
+    static const QString PASSWORD_KEY;
+    static const QString TIMESTAMP_KEY;
+    static const QString REQ_TOKEN_KEY;
+    static const QString MESSAGE_KEY;
 };
 
 #endif // SNAPCHAT_H
