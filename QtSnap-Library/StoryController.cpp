8aa1cdb952ba8d7c18cfa1b2853ea1b1a26d6ad4
James St-Pierre <Jamesst20@gmail.com>
2015-01-12 13:29:10 -0500

    Send stories

--- QtSnap-Library/StoryController.cpp
+++ QtSnap-Library/StoryController.cpp
@@ -3,12 +3,26 @@
 #include "Snapchat.h"
 
 const QString StoryController::STORY_BLOB_PATH = "bq/story_blob";
+const QString StoryController::RETRY_POST_STORY_PATH = "bq/retry_post_story";
 
 const QString StoryController::MY_STORIES_KEY = "my_stories";
 const QString StoryController::FRIEND_STORIES_KEY = "friend_stories";
 const QString StoryController::STORIES_KEY = "stories";
 
-const QString StoryController::STORY_ID = "story_id";
+const QString StoryController::STORY_ID_KEY = "story_id";
+const QString StoryController::CLIENT_ID_KEY = "client_id";
+const QString StoryController::STORY_TIMESTAMP_KEY = "story_timestamp";
+const QString StoryController::GROUP_IDS_KEY = "group_ids";
+const QString StoryController::MEDIA_ID_KEY = "media_id";
+const QString StoryController::ZIPPED_KEY = "zipped";
+const QString StoryController::MY_STORY_KEY = "my_story";
+const QString StoryController::USERNAME_KEY = "username";
+const QString StoryController::CAPTION_TEXT_DISPLAY_KEY = "caption_text_display";
+const QString StoryController::DATA_KEY = "data";
+const QString StoryController::TIME_KEY = "time";
+const QString StoryController::TYPE_KEY = "type";
+const QString StoryController::TIMESTAMP_KEY = "timestamp";
+const QString StoryController::REQ_TOKEN_KEY = "req_token";
 
 StoryController::StoryController()
 {
@@ -56,7 +70,7 @@ void StoryController::getStory(Story story, int requestID){
     storyInfo.mediaIV = story.getMediaIV();
     //Request parameters
     QHash<QString, QString> params;
-    params.insert(STORY_ID, story.getMediaID());
+    params.insert(STORY_ID_KEY, story.getMediaID());
     //Add to queue
     this->queuedStoryInfo.insertMulti(requestID, storyInfo);
     //Execute download request
@@ -75,4 +89,35 @@ void StoryController::onStoryDownloadFinished(int requestID, int httpCode, QByte
     }
     //Remove from queue
     this->queuedStoryInfo.remove(requestID);
+}
+
+void StoryController::sendStory(QString caption, float time, QFile *file, bool isVideo, int requestID, bool videoSound){
+    if(file){
+        if(!file->isOpen()){
+            file->open(QFile::ReadOnly);
+        }
+        if(file->isOpen() && file->isReadable()){
+            QList<QHttpPart> params;
+            qint64 timestamp = QDateTime::currentMSecsSinceEpoch();
+            QString mediaID = Encryption::randomMediaID(this->snapchatInstance->getUsername());
+            params.append(Utils::makeHttpPart(CLIENT_ID_KEY, mediaID));
+            params.append(Utils::makeHttpPart(MEDIA_ID_KEY, mediaID));
+            params.append(Utils::makeHttpPart(GROUP_IDS_KEY, QString("[]")));
+            params.append(Utils::makeHttpPart(ZIPPED_KEY, QString("0")));
+            params.append(Utils::makeHttpPart(MY_STORY_KEY, QString("true")));
+            params.append(Utils::makeHttpPart(USERNAME_KEY, this->snapchatInstance->getUsername()));
+            params.append(Utils::makeHttpPart(CAPTION_TEXT_DISPLAY_KEY, caption));
+            params.append(Utils::makeHttpPart(DATA_KEY, Encryption::encryptSnapOrStory(file->readAll()), true));
+            params.append(Utils::makeHttpPart(TIME_KEY, QString::number(time)));
+            params.append(Utils::makeHttpPart(TYPE_KEY, QString::number((isVideo && videoSound) ? (VIDEO) : (isVideo && !videoSound ? (VIDEO_NOAUDIO) : (IMAGE)))));
+            params.append(Utils::makeHttpPart(TIMESTAMP_KEY, QString::number(timestamp)));
+            params.append(Utils::makeHttpPart(REQ_TOKEN_KEY, TokenLib::getRequestToken(this->snapchatInstance->getAuthToken(), timestamp)));
+            //TODO : STORY_TIMESTAMP_KEY, THUMBNAIL_DATA. WITHOUT THOSE, THERE ARE RISK OF BANISHMENT.
+            this->snapchatInstance->getNetworkRequestMaker().executeRequest(requestID, RETRY_POST_STORY_PATH, params, std::bind(&onStoryUploadSendFinished, this, _1, _2, _3));
+        }
+    }
+}
+
+void StoryController::onStoryUploadSendFinished(int requestID, int httpCode, QByteArray data){
+    emit storyUploadSendFinished(requestID, httpCode >= 200 && httpCode < 300, data);
 }
--- QtSnap-Library/StoryController.h
+++ QtSnap-Library/StoryController.h
@@ -10,6 +10,7 @@
 #include <QJsonArray>
 
 #include <QList>
+#include <QFile>
 
 class Snapchat; //Forward declaration
 class StoryController : public QObject
@@ -39,11 +40,15 @@ public:
 
     void getStory(Story story, int requestID = rand());
 
+    void sendStory(QString caption, float time, QFile *file, bool isVideo, int requestID = rand(), bool videoSound = true);
+
 private slots:
     void onStoryDownloadFinished(int requestID, int httpCode, QByteArray data);
+    void onStoryUploadSendFinished(int requestID, int httpCode, QByteArray data);
 
 signals:
     void storyDownloadFinished(int requestID, bool success, QByteArray data);
+    void storyUploadSendFinished(int requestID, bool success, QByteArray data);
 
 private:
     Snapchat *snapchatInstance;
@@ -54,12 +59,31 @@ private:
     QHash<int, StoryInfo> queuedStoryInfo;
 
     static const QString STORY_BLOB_PATH;
+    static const QString RETRY_POST_STORY_PATH;
 
     static const QString MY_STORIES_KEY;
     static const QString FRIEND_STORIES_KEY;
     static const QString STORIES_KEY;
 
-    static const QString STORY_ID;
+    static const QString STORY_ID_KEY;
+    static const QString CLIENT_ID_KEY;
+    static const QString STORY_TIMESTAMP_KEY;
+    static const QString GROUP_IDS_KEY;
+    static const QString MEDIA_ID_KEY;
+    static const QString ZIPPED_KEY;
+    static const QString MY_STORY_KEY;
+    static const QString USERNAME_KEY;
+    static const QString CAPTION_TEXT_DISPLAY_KEY;
+    static const QString DATA_KEY;
+    static const QString TIME_KEY;
+    static const QString TYPE_KEY;
+    static const QString TIMESTAMP_KEY;
+    static const QString REQ_TOKEN_KEY;
+
+    //MEDIA TYPE
+    static const int IMAGE = 0;
+    static const int VIDEO = 1;
+    static const int VIDEO_NOAUDIO = 2;
 };
 
 #endif // STORYCONTROLLER_H
